2202c2dca432917f52664f93c7e676c7b0d6826f
Tim Romanowski <timromanowski15@gmail.com>
2022-03-27 18:01:26 +0200

    Add green border if reminder is true

--- src/App.js
+++ src/App.js
@@ -1,46 +1,58 @@
 import Header from './components/Header';
 import styled from 'styled-components';
 import Tasks from './components/Tasks';
-import { useState } from "react";
+import { useState } from 'react';
 
 function App() {
-  const [tasks, setTasks] = useState([{
-    id: 1,
-    text: 'doctor apointment',
-    day: 'Feb 5th at 02:30pm',
-    reminder: true,
-},
-{
-    id: 2,
-    text: 'meeting',
-    day: 'Mar 2nd at 01:00pm',
-    reminder: true,
-},
-{
-    id: 3,
-    text: 'Birthday',
-    day: ' Mar 22nd at 08:00pm',
-    reminder: false,
-},
-])
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'doctor apointment',
+      day: 'Feb 5th at 02:30pm',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'meeting',
+      day: 'Mar 2nd at 01:00pm',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Birthday',
+      day: ' Mar 22nd at 08:00pm',
+      reminder: false,
+    },
+  ]);
 
-// DELETE FUNCTION
+  // DELETE FUNCTION
 
   function handleDelete(id) {
-    setTasks(tasks.filter(task => task.id !== id))
+    setTasks(tasks.filter(task => task.id !== id));
   }
 
-// TOGGLE REMINDER FUNCTION
+  // TOGGLE REMINDER FUNCTION
 
   function handleToggleReminder(id) {
-    setTasks(tasks.map(task => task.id === id ? {...task, reminder: !task.reminder} : task))
-  } 
-
+    setTasks(
+      tasks.map(task =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  }
 
   return (
     <Container>
       <Header />
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={handleDelete} onToggle={handleToggleReminder} /> : 'No tasks planned!'}
+      {tasks.length > 0 ? (
+        <Tasks
+          tasks={tasks}
+          onDelete={handleDelete}
+          onToggle={handleToggleReminder}
+        />
+      ) : (
+        'No tasks planned!'
+      )}
     </Container>
   );
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,24 +1,31 @@
-import styled from "styled-components"
-import {FaTimes} from 'react-icons/fa';
+import styled, { css } from 'styled-components';
+import { FaTimes } from 'react-icons/fa';
 
-export default function Task({task, onDelete, onToggle}) {
+export default function Task({ task, onDelete, onToggle }) {
   return (
-    <TaskCard onDoubleClick={() => onToggle(task.id)}>
-        <h3>{task.text} <FaTimes onClick={() => onDelete(task.id)} style={{ color: 'red', cursor:'pointer'}}/></h3>
-        <p>{task.day}</p>
+    <TaskCard reminder={task.reminder} onDoubleClick={() => onToggle(task.id)}>
+      <h3>
+        {task.text}{' '}
+        <FaTimes
+          onClick={() => onDelete(task.id)}
+          style={{ color: 'red', cursor: 'pointer' }}
+        />
+      </h3>
+      <p>{task.day}</p>
     </TaskCard>
-  )
+  );
 }
 
 const TaskCard = styled.section`
-background: #f4f4f4;
-margin: 5px;
-padding: 10px 20px;
-/*cursor: pointer;*/
+  background: #f4f4f4;
+  margin: 5px;
+  padding: 10px 20px;
+  ${props => (props.reminder ? 'border-left: 5px solid green;' : '')}
+  /*cursor: pointer;*/
 
 h3 {
     display: flex;
     align-items: center;
     justify-content: space-between;
-}
-`
+  }
+`;
